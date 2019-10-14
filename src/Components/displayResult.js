7eecdbe88c4524813bec76295edadbcbfa22a567
miyesier <miyesier@gmail.com>
2019-10-14 13:03:02 +0200

    Imports BMICalculation to displayresult componment.

--- src/Components/displayResult.js
+++ src/Components/displayResult.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import { bmiCalculation } from '../Modules/BMICalculator';
 class DisplayResult extends Component {
   calculate(){
     var weight= this.props.weight;
     var height= this.props.height;
+
+    return bmiCalculation(weight, height);
   }
   render(){
     return(
