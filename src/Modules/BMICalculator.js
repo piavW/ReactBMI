2c70cc4ef5c2eba65fe5733ee9f366e09c1648b2
piavw <piavonwachenfelt@hotmail.com>
2019-10-14 12:58:14 +0200

    Creates BMICalculator module, adds metric-BMI calculation function and BMIMessage function.

---
+++ src/Modules/BMICalculator.js
@@ -0,0 +1,33 @@
+export const bmiCalculation = (weight, height, method) => {
+  parseFloat(weight);
+  parseFloat(height);
+  let bmi;
+
+  weight = isNaN(weight) ? 0 : weight;
+  height = isNaN(height) ? 0 : height;
+
+  bmi = weight / (height / 100 * height / 100);
+  
+  let finalBMI = parseFloat(bmi.toFixed(2));
+  let BMIMessage = setBMIMessage(finalBMI)
+  if (isNaN(finalBMI) || !isFinite(finalBMI) || finalBMI === 0) {
+    return 'Please input a number'
+  } else {
+    return `You are ${BMIMessage} with a BMI of ${finalBMI}`;
+  }
+}
+
+const setBMIMessage = (finalBMI) => {
+  if (finalBMI < 18.5) {
+    return 'Underweight';
+  }
+  if (finalBMI > 18.5 && finalBMI < 25) {
+    return 'Normal';
+  }
+  if (finalBMI > 25 && finalBMI < 30) {
+    return 'Overweight';
+  }
+  if (finalBMI > 30) {
+    return 'Obese';
+  }
+}
