5199da59a5bdb74ede53546702f5b514b99a2660
miyesier <miyesier@gmail.com>
2019-10-14 11:23:53 +0200

    Creates input fields for weight and height.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+      <div>
+        <label>Weight(kg)</label>
+        <input name="weight" />
       </div>
+
+      <div>
+          <label>Height(cm)</label>
+          <input name="height" />
+      </div>
+    </div>
     );
   }
 }
